768e95004a34945cd80c4249f2d99d54afb0859f
Long Nguyen <long.nguyen11288@gmail.com>
2015-08-02 14:47:55 -0400

    removed writing file

--- ci/scripts/run_tests.sh
+++ ci/scripts/run_tests.sh
@@ -4,14 +4,6 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR/../..
 
-cat > ~/.lockpick << EOF
-app_id: foobar
-user_id: myuserid
-key: mykeyid
-output_file: ~/mygitcrypt.key
-vault_address: "https://127.0.0.1:8200"
-EOF
-
 godep restore
 
 go vet -x ./...
