8f7e665185f2fd1fed395fd63f009168e8738227
Long Nguyen <long.nguyen11288@gmail.com>
2015-08-02 11:55:14 -0400

    fixed pipeline to work

---
+++ ci/scripts/run_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# change to root of release
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd $DIR/../..
+
+go vet -x ./...
+golint ./...
+go test -v ./...
--- ci/scripts/shipit.sh
+++ ci/scripts/shipit.sh
@@ -6,30 +6,6 @@ set -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR/../..
 
-cat > ~/.bosh_config << EOF
----
-aliases:
-  target:
-    bosh-lite: ${bosh_target}
-auth:
-  ${bosh_target}:
-    username: ${bosh_username}
-    password: ${bosh_password}
-EOF
-bosh target ${bosh_target}
-
-cat > config/private.yml << EOF
----
-blobstore:
-  s3:
-    access_key_id: ${aws_access_key_id}
-    secret_access_key: ${aws_secret_access_key}
-EOF
-
-_bosh() {
-  bosh -n $@
-}
-
 set -e
 
 version=$(cat ../version/number)
@@ -44,23 +20,17 @@ fi
 
 echo Prepare github release information
 set -x
-mkdir -p release
-cp ci/release_notes.md release/notes.md
-echo "${release_name} v${version}" > release/name
-echo "v${version}" > release/tag
-cat > release/slack_success_message.txt <<EOS
-<!here> New version v${version} released
-EOS
+mkdir -p releases
+cp ci/release_notes.md releases/notes.md
+echo "${release_name} v${version}" > releases/name
+echo "v${version}" > releases/tag
 
 git config --global user.email "drnic+bot@starkandwayne.com"
 git config --global user.name "Stark and Wayne CI Bot"
 
 git merge --no-edit ${promotion_branch}
 
-
-bosh target ${BOSH_TARGET}
-
-bosh -n create release --final --with-tarball --version "$version"
+goxc -bc="linux,!arm darwin,amd64" -d=releases -pv=${version}
 
 git add -A
 git commit -m "release v${version}"
